d826115f2334e9c0a46d8664c605db0729d45559
Aleksandra-Danilova <76550825+Aleksandra-Danilova@users.noreply.github.com>
2021-02-11 16:53:54 +0300

    Add files via upload
    Display all foreign key relationships between schema tables, along with the number of users who have privileges to delete rows from the main table, and the rule for deleting rows from the child table when deleting rows from the main table.

---
+++ data_dictionary.sql
@@ -0,0 +1,18 @@
+﻿SELECT
+    ucc.table_name || '.' || ucc.column_name AS "CUST.FK",
+    ucc2.table_name || '.' || ucc2.column_name AS "SUPP.PK",
+    COUNT(DISTINCT utpm.grantee) AS us_del_cnt, --количество пользователей, имеющих привилегии на удаление строк из главной таблицы
+    nvl(uc.delete_rule, ' ') AS del_rule --правило удаления строк
+FROM
+    user_constraints uc
+    JOIN user_cons_columns ucc ON uc.constraint_name = ucc.constraint_name --для определения FK (дочерняя таблица)
+    JOIN user_cons_columns ucc2 ON uc.r_constraint_name = ucc2.constraint_name --для определения PK (главная таблица)
+                                   AND ucc.position = ucc2.position
+    LEFT JOIN user_tab_privs_made utpm ON ucc2.table_name = utpm.table_name
+WHERE
+    uc.constraint_type = 'R'
+GROUP BY 
+    uc.constraint_type,
+    ucc.table_name || '.' || ucc.column_name,
+    ucc2.table_name || '.' || ucc2.column_name,
+    uc.delete_rule;
