ef55e78aa75725c6d0527cafa1370d86257e8b89
Aleksandra-Danilova <76550825+Aleksandra-Danilova@users.noreply.github.com>
2022-04-29 10:19:59 +0300

    Sort Words

--- sort_words.sql
+++ sort_words.sql
@@ -1,23 +1,24 @@
 ﻿DEFINE str = 'abc,cde,ef,gh,mn,test,ss,df,fw,ewe,wwe';
 
 WITH 
---преобразование строки в столбец
+--transform a row to a column
 str_to_column AS ( 
-    SELECT REGEXP_SUBSTR('&str', '[^,]+', 1, LEVEL) AS col --извлекаем из строки подстроки по запятым; 1-позиция, с которой надо искать, LEVEL-номер вхождения
+    --extract comma-separated substrings from the string; 1 - position from which to search, LEVEL - number of occurrence
+    SELECT REGEXP_SUBSTR('&str', '[^,]+', 1, LEVEL) AS col
     FROM dual
-    CONNECT BY LEVEL <= REGEXP_COUNT('&str', ',') + 1), --уровней (строк в столбце) на один больше запятых в строке
---сортировка по алфавиту    
+    CONNECT BY LEVEL <= REGEXP_COUNT('&str', ',') + 1), --there are one more levels (rows in a column) than there are commas in a row
+--sort alphabetically   
 alphabet_sort AS ( 
     SELECT col, ROWNUM AS r
     FROM( SELECT col
           FROM str_to_column
-          WHERE REGEXP_LIKE(col, '^[[:alpha:]]+$') --оставляем только элементы с буквами
+          WHERE REGEXP_LIKE(col, '^[[:alpha:]]+$') --leave only elements with letters
           ORDER BY col))
 --преобразование столбца в строку
-SELECT '&str' AS "Исходная строка", LTRIM(SYS_CONNECT_BY_PATH(col, ','), ',') AS "Результат" --удаляем в начале строки запятую
+SELECT '&str' AS "Original string", LTRIM(SYS_CONNECT_BY_PATH(col, ','), ',') AS "Result" --delete the comma at the beginning of the line
     FROM (SELECT col, ROWNUM r FROM alphabet_sort)
     WHERE r = (SELECT COUNT(*) FROM alphabet_sort)
     START WITH r = 1
-    CONNECT BY PRIOR r = r - 1; --подключаем к более низкому уровню значение на следующем уровне
+    CONNECT BY PRIOR r = r - 1; --connect the value at the next level to a lower level
     
 UNDEFINE str;
