f5fe408dec4af645aaf542382e8594da01161c7b
Aleksandra-Danilova <76550825+Aleksandra-Danilova@users.noreply.github.com>
2022-05-01 16:29:28 +0300

    Date of Elenctions

---
+++ election_date/election_date.sql
@@ -0,0 +1,20 @@
+SELECT
+    '&given_date' AS "Input date",
+    CASE
+            --if the year is not a multiple of 4
+            WHEN mod(substr(TO_CHAR('&given_date'),7),4) != 0 THEN TO_CHAR( (next_day(TO_DATE('31.10.'
+            || TO_CHAR(substr(TO_CHAR('&given_date'),7) + 4 - mod(substr(TO_CHAR('&given_date'),7),4) ) ),1) + 1),'DD.MM.YYYY')
+            --if the year is a multiple of 4, the date before and on the election day of that year
+            WHEN mod(substr(TO_CHAR('&given_date'),7),4) = 0
+                 AND '&given_date' <= next_day(TO_DATE('31.10.'
+            || TO_CHAR(substr(TO_CHAR('&given_date'),7) ) ),1) + 1 THEN TO_CHAR( (next_day(TO_DATE('31.10.'
+            || TO_CHAR(substr(TO_CHAR('&given_date'),7) ) ),1) + 1),'DD.MM.YYYY')
+            --if the year is a multiple of 4, the date after the election of this year
+            WHEN mod(substr(TO_CHAR('&given_date'),7),4) = 0
+                 AND '&given_date' > next_day(TO_DATE('31.10.'
+            || TO_CHAR(substr(TO_CHAR('&given_date'),7) ) ),1) + 1 THEN TO_CHAR( (next_day(TO_DATE('31.10.'
+            || TO_CHAR(substr(TO_CHAR('&given_date'),7) + 4) ),1) + 1),'DD.MM.YYYY')
+        END
+    AS "Election date"
+FROM
+    dual;    
